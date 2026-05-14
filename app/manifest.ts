ce8f8af8f55c40038a1b588482335b9a01bb68a3
Andy Cherenfant <andycherenfant94@gmail.com>
2026-05-13 20:47:36 -0400

    fix: update manifest icon purpose to 'maskable' for better PWA compatibility

--- app/manifest.ts
+++ app/manifest.ts
@@ -25,13 +25,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/api/icon",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
       {
         src: "/api/icon",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "maskable",
       },
     ],
   };
